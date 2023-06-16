de8e384a8bd479bfb3fd13c685f0312355ed4724
mungvt <vutienmung@gmail.com>
2023-06-16 13:57:24 +0700

    chore: change config default

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 
 
 class AppConfig:
-    HOST = os.environ.get("HOST") or "0.0.0.0"
-    PORT = int(os.environ.get("PORT")) or 8000
-    LIMIT_ORDERS = int(os.environ.get("LIMIT_ORDERS")) or 10
+    HOST = os.environ.get("HOST", "0.0.0.0")
+    PORT = int(os.environ.get("PORT", 3333))
+    LIMIT_ORDERS = int(os.environ.get("LIMIT_ORDERS", 10))
     BASE_URL = f"http://{HOST}:{PORT}"
 
 
 class BackendConfig:
-    RETRY_DELAY = int(os.environ.get("RETRY_DELAY")) or 10
-    WEBSOCKET_URL = os.environ.get("WEBSOCKET_URL") or "wss://socket.delta.exchange"
-    PRODUCTS = os.environ.get("PRODUCTS").split(",") or ["C-BTC-26000-230623", "P-BTC-26000-230623"]
-    HEARTBEAT_TIME = int(os.environ.get("HEARTBEAT_TIME")) or 3
-    DATA_DIR = os.environ.get("DATA_DIR") or "./data/"
+    RETRY_DELAY = int(os.environ.get("RETRY_DELAY", 10))
+    WEBSOCKET_URL = os.environ.get("WEBSOCKET_URL", "wss://socket.delta.exchange")
+    PRODUCTS = os.environ.get("PRODUCTS", "C-BTC-26000-230623, P-BTC-26000-230623").split(",")
+    HEARTBEAT_TIME = int(os.environ.get("HEARTBEAT_TIME", 3))
+    DATA_DIR = os.environ.get("DATA_DIR", "./data/")
